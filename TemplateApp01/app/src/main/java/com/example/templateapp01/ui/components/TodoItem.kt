6e23c041cbe09e7f4a1cf7a6b5f407f4a4c50f61
LeoAndo <leo.777.info@gmail.com>
2022-02-19 23:27:43 +0900

    replace material3 components

--- TemplateApp01/app/src/main/java/com/example/templateapp01/ui/components/TodoItem.kt
+++ TemplateApp01/app/src/main/java/com/example/templateapp01/ui/components/TodoItem.kt
@@ -5,10 +5,10 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -54,11 +54,11 @@ internal fun TodoItem(
             }
             Text(
                 text = "Title: " + todoData.title,
-                style = MaterialTheme.typography.subtitle1
+                style = MaterialTheme.typography.bodyMedium
             )
             Text(
                 text = "Memo: " + todoData.memo,
-                style = MaterialTheme.typography.subtitle1
+                style = MaterialTheme.typography.bodyMedium
             )
         }
     }
